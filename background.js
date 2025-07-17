2d2ff0bfc628b3ae61dbeaeb4146659f58c50778
Noah <noah.coffman@macdiscount.com>
2025-07-17 13:13:51 -0400

    Update background.js

--- background.js
+++ background.js
@@ -4,3 +4,16 @@ chrome.action.onClicked.addListener((tab) => {
     files: ['content.js']
   });
 });
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message.type === 'modelCopied') {
+    const tabId = sender.tab.id;
+
+    chrome.action.setBadgeText({ tabId, text: '✔️' });
+    chrome.action.setBadgeBackgroundColor({ tabId, color: '#4caf50' });
+
+    setTimeout(() => {
+      chrome.action.setBadgeText({ tabId, text: '' });
+    }, 2000);
+  }
+});
